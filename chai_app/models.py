695518f6261f7a7c4274fd96f5b67ada90cc6ff8
AnkitB92 <ankit92b@gmail.com>
2024-10-14 20:38:31 +0530

    added new models

--- chai_app/models.py
+++ chai_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -26,3 +27,33 @@ class ChaiVariety(models.Model):
 
   def __str__(self):
     return self.name
+
+
+class ChaiReview(models.Model):
+  chai = models.ForeignKey(ChaiVariety, on_delete=models.CASCADE, related_name='reviews')
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  rating = models.IntegerField()
+  comment = models.TextField()
+  date_added = models.DateTimeField(default=timezone.now)
+  
+  def __str__(self) -> str:
+    return f"{self.user.username}"
+
+
+class Store(models.Model):
+  name = models.CharField(max_length=100)
+  location = models.CharField(max_length=100)
+  chai_variety = models.ManyToManyField(ChaiVariety, related_name='stores')
+  
+  def __str__(self):
+      return self.name
+
+
+class ChaiCertificate(models.Model):
+  chai = models.OneToOneField(ChaiVariety, on_delete=models.CASCADE, related_name='certificate')
+  serial_number = models.CharField(max_length=50)
+  issued_date = models.DateTimeField(default=timezone.now)
+  valid_till = models.DateTimeField()
+  
+  def __str__(self):
+      return f'Certificate for {self.chai.name}'
