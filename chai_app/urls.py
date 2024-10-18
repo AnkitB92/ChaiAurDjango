6f39f6f57ea2ac0a370d046de00c4143e3033d7f
AnkitB92 <ankit92b@gmail.com>
2024-10-18 16:39:29 +0530

    format document for urls.py

--- chai_app/urls.py
+++ chai_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-  path('', views.chai_app, name='chai_app'),
-  path('<int:chai_id>/', views.chai_details, name='chai_details'),
+urlpatterns = [
+    path("", views.chai, name="chai"),
+    path("<int:chai_id>/", views.chai_details, name="chai_details"),
+    path("stores/", views.chai_store_view, name="stores"),
 ]
